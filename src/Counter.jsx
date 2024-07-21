e59c980b7ae493708d32f875a185387a6910e1b8
Kartikds09 <0912kartik@gmail.com>
2024-07-21 14:26:31 +0530

    file chnges

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,9 +1,39 @@
 import React,{useState} from "react";
 
 function Counter(){
+    const [count, setCount] = useState(0);
+
+    function increment(){
+        // setCount(count+1);
+
+        //using updater which is better as updates variable also
+        setCount(c => c+1);
+        
+        setCount(c => c+1);
+        setCount(c => c+1);
+    }
+
+    function decrement(){
+        // setCount(count -1);
+
+        //updater
+        setCount(c => c -1);
+
+        setCount(c => c -1);
+        setCount(c => c -1);
+    }
+
+    function reset(){
+        setCount(0);
+    }
     return(
         <>
-        
+            <div className='counter-container'>
+                <p className="count-display">{count}</p>
+                <button className="counter-button" onClick={decrement}>Decrease</button>
+                <button className="counter-button" onClick={reset}>Reset Counter</button>
+                <button className="counter-button" onClick={increment}>Increase</button>
+            </div>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AppProps from './App_props.jsx'
-import AppOnClick from './App_onClick.jsx'
-import AppMyComponent from './App_myComponent.jsx'
-import App from './App.jsx'
-import './index.css'
-import './Counter.css'
-import App_onChange from './App_onChange.jsx'
-import App_ColorPicker from './App_ColorPicker.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppProps from "./App_props.jsx";
+import AppOnClick from "./App_onClick.jsx";
+import AppMyComponent from "./App_myComponent.jsx";
+import App_counter from "./App_counter.jsx";
+import "./index.css";
+import "./Counter.css";
+import App_onChange from "./App_onChange.jsx";
+import App_ColorPicker from "./App_ColorPicker.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <AppProps /> */}
     {/* <AppOnClick/> */}
     {/* <AppMyComponent/>
-     */}      
-     {/* <App_onChange/> */}
+     */}
+    {/* <App_onChange/> */}
 
-      <App_ColorPicker/>
-
-  </React.StrictMode>,
-)
+    {/* <App_ColorPicker/> */}
+    {/* <App_counter/> */}
+  </React.StrictMode>
+);
